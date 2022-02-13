2dff786d674a6b20788ffe6d4dc86f45013c7742
Girish Parate <girishparate41@gmail.com>
2022-02-13 18:18:29 +0530

    Initial Commit

---
+++ reqres_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reqres_swiftui
+//
+//  Created by Girish Parate on 13/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ reqres_swiftui/reqres_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  reqres_swiftuiApp.swift
+//  reqres_swiftui
+//
+//  Created by Girish Parate on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct reqres_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
